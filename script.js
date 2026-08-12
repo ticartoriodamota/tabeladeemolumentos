e2a2ae413760756ad2bd9eefed83a9a4e93dcbf0
ticartoriodamota <ticartoriodamota@gmail.com>
2026-08-12 15:47:34 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const botoes = document.querySelectorAll(".botao");
+
+    botoes.forEach((botao, index) => {
+
+        botao.style.opacity = "0";
+        botao.style.transform = "translateY(15px)";
+
+        setTimeout(() => {
+
+            botao.style.transition =
+                "opacity 0.5s ease, transform 0.5s ease";
+
+            botao.style.opacity = "1";
+            botao.style.transform = "translateY(0)";
+
+        }, 100 + (index * 100));
+
+    });
+
+});
